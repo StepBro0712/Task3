8b6461584db22d2e3808531ff4cef48360013290
StepBro0712 <stepa.super2021@mail.ru>
2023-03-17 14:43:33 +0700

    form

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,97 +1,23 @@
 let eventBus = new Vue();
 
-Vue.component('notes', {
-    props: {
-        note: {
-            name: {
-                type: Text,
-                required: true
-            },
-            tasks: {
-                type: Array,
-                required: true,
-                readiness: {
-                    type: Boolean,
-                    required: true
-                }
-            },
-            status: {
-                type: Number,
-                required: true
-            },
-        },
-    },
-    template: `
-        <div class="notes">
-            
-            <newNotes></newNotes>
-            <h2 class="error" v-for="error in errors">{{error}}</h2>
-            <div class="note-wrap">
-                <div class="note">
-                 <h1>Запланированные задачи</h1>
-                    <ul>
-                        <li class="notes-li" v-for="note in column_1"><p class="note-name">{{note.name}}</p>
-                            <ul>
-                                <li class="tasks" v-for="task in note.tasks" v-if="task.name !== null"">
-                                <p class="p-li" :class="{ textDecoration: task.readiness }">{{task.name}}</p>
-                                <input type="checkbox" class="checkbox" @click="newStatus_1(note, task)" :disabled="task.readlines">
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="note">
-                    <h1>Задачи в работе</h1>
-                    <ul>
-                        <li class="notes-li" v-for="note in column_2"><p class="note-name">{{note.name}}</p>
-                            <ul>
-                                <li class="tasks-2" v-for="task in note.tasks" v-if="task.name !== null">
-                                <input type="checkbox" class="checkbox" @click="newStatus_2(note, task)" :disabled="task.readlines>
-                                <p class="p-li" :class="{ textDecoration: task.readiness }">{{task.name}}</p>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>  
-                <div class="note">
-                <h1>Тестирование</h1>
-                    <ul>
-                        <li class="notes-li" v-for="note in column_2"><p class="note-name">{{note.name}}</p>
-                            <ul>
-                                <li class="tasks-3" v-for="task in note.tasks" v-if="task.name !== null">
-                                <input type="checkbox" class="checkbox" @click="newStatus_3(note, task)" :disabled="task.readlines">
-                                <p class="p-li" :class="{ textDecoration: task.readiness }">{{task.name}}</p>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>  
-                <div class="note">
-                    <h1>Выполненные задачи</h1>
-                    <ul>
-                        <li class="notes-li" v-for="note in column_3">
-                            <p class="note-name">{{note.name}}</p>
-                            <ul>
-                                <li class="tasks-4" v-for="task in note.tasks" v-if="task.name !== null">
-                                <input type="checkbox" class="checkbox" @click="task.readiness = true" :disabled="task.readlines">
-                                <p class="p-li" :class="{ textDecoration: task.readiness }">{{task.name}}</p>
-                                </li>
-                                <p class="note-data">{{ note.date }}</p>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+Vue.component('board', {
+    template:`
+        <div class="tabs">
+            <newBoard></newBoard>
+            <div class="tabs-wrap">
+                <table_1 :column_1="column_1"></table_1>
+                <table_2 :column_2="column_2"></table_2>
+                <table_3 :column_3="column_3"></table_3>
+                <table_4 :column_4="column_4"></table_4>
+            </div>   
         </div>
     `,
-    data() {
-        return {
-            column_1: [],
-            column_2: [],
-            column_3: [],
-            column_4: [],
-            errors: [],
-            active: 0
+    data(){
+        return{
+            column_1:[],
+            column_2:[],
+            column_3:[],
+            column_4:[],
         }
     },
     mounted() {
@@ -99,15 +25,25 @@ Vue.component('notes', {
         this.column_2 = JSON.parse(localStorage.getItem("column_2")) || [];
         this.column_3 = JSON.parse(localStorage.getItem("column_3")) || [];
         this.column_4 = JSON.parse(localStorage.getItem("column_4")) || [];
-        eventBus.$on('notes-submitted', note => {
-            this.errors = []
-            if (this.column_1.length < 3) {
-                this.column_1.push(note);
-                this.saveNote_1();
-            } else {
-                this.errors.push('Maximum number of tasks!');
+        eventBus.$on('addColumn_1', tab => {
+            this.column_1.push(tab);
+            this.saveTab_1();
+        });
+        eventBus.$on('addColumn_2', tab => {
+            this.column_2.push(tab);
+            this.saveTab_2();
+        });
+        eventBus.$on('addColumn_3', tab => {
+            this.column_3.push(tab);
+            this.saveTab_3();
+        });
+        eventBus.$on('addColumn_4', tab => {
+            this.column_4.push(tab);
+            if (tab.date > tab.deadline){
+                tab.term = false;
             }
-        })
+            this.saveTab_4();
+        });
     },
     watch: {
         column_1(newValue) {
@@ -123,16 +59,16 @@ Vue.component('notes', {
             localStorage.setItem("column_4", JSON.stringify(newValue));
         },
         methods: {
-            saveNote_1() {
+            saveTab_1() {
                 localStorage.setItem('column_1', JSON.stringify(this.column_1));
             },
-            saveNote_2() {
+            saveTab_2() {
                 localStorage.setItem('column_2', JSON.stringify(this.column_2));
             },
-            saveNote_3() {
+            saveTab_3() {
                 localStorage.setItem('column_3', JSON.stringify(this.column_3));
             },
-            saveNote_4() {
+            saveTab_4() {
                 localStorage.setItem('column_4', JSON.stringify(this.column_4));
             },
         }
@@ -140,23 +76,27 @@ Vue.component('notes', {
 })
 
 
-Vue.component( 'newNotes', {
+Vue.component( 'newTab', {
     template: `
-            <div class="create-window">
-                <div class="modal-header">
-                    <div class="create-body">
-                        <div class="create_form">
-                            <form class="create" @submit.prevent="onSubmit">
-                                <input id="name" v-model="name" type="text" placeholder="Название задачи" required maxlength="20">
-                                <input id="date" type="date">
-                                <textarea id="text" placeholder="Описание"></textarea>
-                                <input id="deadline" type="date">>
-                                <button type="submit">Create</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>    
-            </div> 
+        <button type="button" class="button" @click="show=true">Создать задачу</button>
+            <slot name="header">
+            <button class="close" type="button" @click="close">×</button>
+            </slot>
+            </div>
+                <slot name="body">
+                    <div class="create_form">
+                        <form class="create" @submit.prevent="onSubmit">
+                            <input id="title" v-model="title" type="text" placeholder="Название">   
+                            <label for="description" required>Описание</label>
+                            <textarea id="text" required></textarea>
+                            <input id="deadline" type="date" required>        
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                            <button type="submit">Создать</button>
+                        </form>
+                   </div>
+               </slot>  
         `,
     data() {
         return {
